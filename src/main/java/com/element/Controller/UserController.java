ad6e3ef37e34ac9fb17dd25874e0ff4241b1aa02
747164090 <747164090@qq.com>
2020-09-23 18:29:23 +0800

    1

--- src/main/java/com/element/Controller/UserController.java
+++ src/main/java/com/element/Controller/UserController.java
@@ -1,5 +1,6 @@
 package com.element.Controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.element.Entity.po.User;
 import com.element.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,30 +15,37 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/login")
-    public User login(@RequestParam("userName") String userName, @RequestParam("password") String password){
+    public String login1(@RequestParam("userName") String userName, @RequestParam("password") String password){
         User login = userService.login(userName,password);
         if(login!=null){
-            return login;
+            JSONObject result = new JSONObject();
+            result.put("status","success");
+            result.put("detail",login);
+            return  result.toJSONString();
         }else{
-            return null;
+            JSONObject result = new JSONObject();
+            result.put("status","failure");
+            result.put("detail","账户名或密码错误");
+            return  result.toJSONString();
         }
     }
-
     //注册功能
     @PostMapping("/register")
-    public ModelAndView register(@RequestParam("userId") String userId, @RequestParam("userName")
+    public String register(@RequestParam("userId") String userId, @RequestParam("userName")
             String userName, @RequestParam("password") String password,@RequestParam("userSex") int userSex, @RequestParam("userImg")
             String userImg, @RequestParam("delTag") int delTag){
-        ModelAndView mv = new ModelAndView();
+        JSONObject result = new JSONObject();
         //用户或密码为空的条件判断
         if(userId.isEmpty()||userName.isEmpty()||password.isEmpty()){
-            mv.setViewName("register");
-            mv.addObject("tip1","用户或密码不能为空");
-            return mv;
+            JSONObject result1 = new JSONObject();
+            result1.put("status","failure");
+            result1.put("detail","用户名或密码不能为空");
+            return  result1.toJSONString();
         }
         //判断是否取到用户，如果没有就保存在数据库中
         User user= userService.findUserByUserName(userName);
-        if(user == null){
+        User user1 = userService.findUserByUserId(userId);
+        if(user == null && user1 == null){
             User register=new User();
             register.setUserId(userId);
             register.setUserName(userName);
@@ -46,12 +54,12 @@ public class UserController {
             register.setUserImg(userImg);
             register.setDelTag(delTag);
             userService.save(register);
-            mv.setViewName("success");
+            result.put("status","success");
+            result.put("detail",register);
+        }else {
+            result.put("status","failure");
+            result.put("detail","还没有商家");
         }
-        else {
-            mv.setViewName("404");
-        }
-        return mv;
+        return  result.toJSONString();
     }
-
 }
